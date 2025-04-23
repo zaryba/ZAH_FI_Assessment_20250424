7aea0b0f024ad47ea157ddb0ed49ad5526edabeb
zaryba <49306772+zaryba@users.noreply.github.com>
2025-04-23 22:19:17 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+import datetime as dt
+
+
+class IndexModel:
+    def __init__(self) -> None:
+        # To be implemented
+        pass
+
+    def calc_index_level(self, start_date: dt.date, end_date: dt.date) -> None:
+        # To be implemented
+        pass
+
+    def export_values(self, file_name: str) -> None:
+        # To be implemented
+        pass
